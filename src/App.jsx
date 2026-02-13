7b8a485423e7ffbc31eac90a15ed837c9323b3d0
Hala <hm1316572@gmail.com>
2026-02-13 23:47:12 +0200

    countdown

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./component/Counter"
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center py-22">
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Counter.jsx
@@ -0,0 +1,79 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import { DiDart } from "react-icons/di";
+
+
+export default function Counter() {
+
+    const [count , setCount] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    });
+
+    useEffect (()=> {
+
+        let countDown = new Date("March 30, 2026 12:24:22").getTime();
+
+        const counter = setInterval(() => {
+            
+            const dateNow = new Date().getTime();
+            const dateDiff = countDown - dateNow
+
+            if(dateDiff <= 0){
+                clearInterval(counter)
+            }
+
+            setCount({
+                days: Math.floor(dateDiff /(1000 * 60 * 60 * 24)),
+                hours: Math.floor(dateDiff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
+                minutes: Math.floor(dateDiff % (1000 * 60 * 60 ) / (1000 * 60 )),
+                seconds: Math.floor(dateDiff % (1000 * 60  ) / (1000  )),
+            })
+
+
+        }, 1000);
+
+        return ()=> clearInterval(counter)
+
+    }, [])
+
+
+
+
+return (
+    <div className="text-center">
+        <div>
+            <h1 className="uppercase text-white font-bold tracking-[6px] pt-9">We're launching soon</h1>
+        </div>
+
+        <div className="flex gap-8 py-19 ">
+            <div className="timer-bomx relative">
+                <h2 className="bg-[hsl(236,21%,26%)] text-red-400 w-22 h-22 flex items-center justify-center text-4xl font-bold rounded-sm">{count.days}</h2>
+                <span className="text-gray-400 uppercase tracking-[4px] text-[11px] font-bold ">days</span>
+            </div>
+            <div className="timer-bomx relative">
+                <h2 className="bg-[hsl(236,21%,26%)] text-red-400 w-22 h-22 flex items-center justify-center text-4xl font-bold rounded-sm">{count.hours}</h2>
+                <span className="text-gray-400 uppercase tracking-[4px] text-[11px] font-bold ">hours</span>
+            </div>
+            <div className="timer-bomx relative">
+                <h2 className="bg-[hsl(236,21%,26%)] text-red-400 w-22 h-22 flex items-center justify-center text-4xl font-bold rounded-sm">{count.minutes}</h2>
+                <span className="text-gray-400 uppercase tracking-[4px] text-[11px] font-bold ">minutes</span>
+            </div>
+            <div className="timer-bomx relative">
+                <h2 className="bg-[hsl(236,21%,26%)] text-red-400 w-22 h-22 flex items-center justify-center text-4xl font-bold rounded-sm">{count.seconds}</h2>
+                <span className="text-gray-400 uppercase tracking-[4px] text-[11px] font-bold ">seconds</span>
+            </div>
+        </div>
+
+        <div className="flex gap-6 text-center mx-auto mt-29 ml-36">
+            <img src="./images/icon-facebook.svg" alt="" />
+            <img src="./images/icon-instagram.svg" alt="" />
+            <img src="./images/icon-pinterest.svg" alt="" />
+        </div>
+
+
+    </div>
+)
+}
